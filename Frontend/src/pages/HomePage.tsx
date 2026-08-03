a6e05d77f3bd17c52012da92bcfaeebd60647c06
Laiba Ijaz <laibaijaz904@gmail.com>
2026-08-03 23:21:05 +0500

    add HomePage component for landing page display

---
+++ Frontend/src/pages/HomePage.tsx
@@ -0,0 +1,27 @@
+import landingImg from "../assets/landing.png"
+import appDownloadImg from '../assets/appDownload.png'
+export default function HomePage() {
+  return (
+    <div className='flex flex-col gap-12'>
+        <div className='bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16'>
+            <h1 className='text-3xl md:text-5xl font-bold tracking-tight text-orange-600'>
+                Tuck into a takeaway today
+            </h1>
+            <span className='text-xl'>Food is just a click away!</span>
+        </div>
+        <div className=' grid md:grid-cols-2 gap-5'>
+            <img src={landingImg} alt="langingimg" />
+            <div className="flex flex-col items-center justify-center gap-4 text-center">
+                <span className="font-bold text-3xl tracking-tight">
+                    Order takeaway even faster!
+                </span>
+                <span>
+                    Download the BiteFlow App for fast ordering and personalised recommendations
+                </span>
+                <img src={appDownloadImg} alt="AppDownloadImg" />
+            </div>
+
+        </div>
+    </div>
+  )
+}
