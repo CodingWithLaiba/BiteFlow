e59f1316ca43c641285cde25ac065765e4f93f73
Laiba Ijaz <laibaijaz904@gmail.com>
2026-08-03 23:18:56 +0500

    add AppRoutes component for routing setup

---
+++ Frontend/src/AppRoutes.tsx
@@ -0,0 +1,21 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layout/layout";
+import HomePage from "./pages/HomePage";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route path="/user-profile" element={<span>UserPage</span>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
+export default AppRoutes;
