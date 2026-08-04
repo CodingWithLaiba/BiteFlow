ba3e88cf72d3ab308b160ebd88b28df53fff0280
Laiba Ijaz <laibaijaz904@gmail.com>
2026-08-04 23:54:56 +0500

    refactor index.ts: improve MongoDB connection logging and organize route imports

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,17 +2,19 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import myUserRoute from "./routes/myUserRoute";
 dotenv.config();
 
-mongoose.connect(process.env.MONGOBD_CONNECTION as string).then(()=>console.log("Connected to MongoDB!"))
+mongoose
+  .connect(process.env.MONGOBD_CONNECTION as string)
+  .then(() => console.log("Connected to MongoDB!"));
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello!" });
-});
+//api/my/user
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
