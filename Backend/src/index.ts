eeef05263d62a402d7c57d178e678ef4e608be68
Laiba Ijaz <laibaijaz904@gmail.com>
2026-08-11 22:01:13 +0500

    feat(index): configure cloudinary and add MyRestaurantRoute integration

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,12 +2,20 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import { v2 as cloudinary } from "cloudinary";
 import myUserRoute from "./routes/myUserRoute";
+import MyRestaurantRoute from './routes/MyRestaurantRoute'
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGOBD_CONNECTION as string)
   .then(() => console.log("Connected to MongoDB!"));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 app.use(express.json());
@@ -15,6 +23,8 @@ app.use(cors());
 
 //api/my/user
 app.use("/api/my/user", myUserRoute);
+
+app.use("/api/my/restaurant", MyRestaurantRoute)
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
