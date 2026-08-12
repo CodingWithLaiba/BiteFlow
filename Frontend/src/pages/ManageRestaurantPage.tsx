d7a1bc6554f16468c501d7ce5ba139da39aa358e
Laiba Ijaz <laibaijaz904@gmail.com>
2026-08-12 23:04:51 +0500

    feat(ManageRestaurantPage): integrate restaurant retrieval in ManageRestaurantPage

--- Frontend/src/pages/ManageRestaurantPage.tsx
+++ Frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,9 +1,17 @@
-import { useCreateMyRestaurant } from '@/api/MyRestaurantApi'
-import ManageRestaurantForm from '@/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm'
+import {
+  useCreateMyRestaurant,
+  useGetMyRestaurant,
+} from "@/api/MyRestaurantApi";
+import ManageRestaurantForm from "@/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm";
 
 export default function ManageRestaurantPage() {
-    const {createRestaurant, isPending} = useCreateMyRestaurant()
+  const { createRestaurant, isPending } = useCreateMyRestaurant();
+  const { restaurant } = useGetMyRestaurant();
   return (
-    <ManageRestaurantForm onSave={createRestaurant} isLoading={isPending} />
-  )
+    <ManageRestaurantForm
+      restaurant={restaurant}
+      onSave={createRestaurant}
+      isLoading={isPending}
+    />
+  );
 }
