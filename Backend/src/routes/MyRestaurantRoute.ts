434513ca050f7d48d826ced06763a1a03ac44d14
Laiba Ijaz <laibaijaz904@gmail.com>
2026-08-14 16:27:00 +0500

    refactor(MyRestaurantRoute): remove commented-out order status update route

--- Backend/src/routes/MyRestaurantRoute.ts
+++ Backend/src/routes/MyRestaurantRoute.ts
@@ -26,7 +26,7 @@ const upload = multer({
 //   jwtCheck,
 //   jwtParse,
 //   MyRestaurantController.updateOrderStatus,
-// );
+// ); 
 
 router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
 
