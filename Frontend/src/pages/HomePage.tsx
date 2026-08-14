66aade0e068380478dc998dd6d2960567e7a7124
Laiba Ijaz <laibaijaz904@gmail.com>
2026-08-14 16:27:59 +0500

    feat(HomePage): integrate search functionality with SearchBar component

--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -1,27 +1,39 @@
-import landingImg from "../assets/landing.png"
-import appDownloadImg from '../assets/appDownload.png'
+import landingImg from "../assets/landing.png";
+import appDownloadImg from "../assets/appDownload.png";
+import SearchBar, { type SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
 export default function HomePage() {
+  const navigate = useNavigate();
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
   return (
-    <div className='flex flex-col gap-12'>
-        <div className='bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16'>
-            <h1 className='text-3xl md:text-5xl font-bold tracking-tight text-orange-600'>
-                Tuck into a takeaway today
-            </h1>
-            <span className='text-xl'>Food is just a click away!</span>
+    <div className="flex flex-col gap-12">
+      <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+        <h1 className="text-3xl md:text-5xl font-bold tracking-tight text-orange-600">
+          Tuck into a takeaway today
+        </h1>
+        <span className="text-xl">Food is just a click away!</span>
+        <SearchBar
+          placeHolder="Search by City or Town"
+          onSubmit={handleSearchSubmit}
+        />
+      </div>
+      <div className=" grid md:grid-cols-2 gap-5">
+        <img src={landingImg} alt="langingimg" />
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
+          <span className="font-bold text-3xl tracking-tight">
+            Order takeaway even faster!
+          </span>
+          <span>
+            Download the BiteFlow App for fast ordering and personalised
+            recommendations
+          </span>
+          <img src={appDownloadImg} alt="AppDownloadImg" />
         </div>
-        <div className=' grid md:grid-cols-2 gap-5'>
-            <img src={landingImg} alt="langingimg" />
-            <div className="flex flex-col items-center justify-center gap-4 text-center">
-                <span className="font-bold text-3xl tracking-tight">
-                    Order takeaway even faster!
-                </span>
-                <span>
-                    Download the BiteFlow App for fast ordering and personalised recommendations
-                </span>
-                <img src={appDownloadImg} alt="AppDownloadImg" />
-            </div>
-
-        </div>
+      </div>
     </div>
-  )
+  );
 }
